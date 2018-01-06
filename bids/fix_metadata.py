2e48012d240a6e16a5ac36a6a9ae6a86b45dc407
poldrack <poldrack@gmail.com>
2018-01-05 17:23:45 -0800

    fixed skip

--- bids/fix_metadata.py
+++ bids/fix_metadata.py
@@ -23,23 +23,23 @@ for sub in layout.get_subjects():
 
         m1=layout.get(subject=sub,session=sess,
             type='magnitude1',extensions='.json')
-        try:
-          assert len(m1)==1
-        except:
-          print('no fieldmap - skipping')
-        m2=layout.get(subject=sub,session=sess,
-            type='magnitude2',extensions='.json')
-        assert len(m2)==1
-        pd=layout.get(subject=sub,session=sess,
-                        type='phasediff',extensions='.json')
-        assert len(pd)==1
-        pd_metadata=layout.get_metadata(pd[0].filename)
-        if 'EchoTime' in pd_metadata:
-            pd_metadata['EchoTime1']=layout.get_metadata(m1[0].filename)['EchoTime']
-            pd_metadata['EchoTime2']=layout.get_metadata(m2[0].filename)['EchoTime']
-            del pd_metadata['EchoTime']
-            json.dump(pd_metadata,open(pd[0].filename,'w'),
-                            sort_keys=True,indent=4)
+        assert len(m1)<=1
+        if len(m1)==0:
+            print('no fieldmap - skipping')
+        else:
+            m2=layout.get(subject=sub,session=sess,
+                type='magnitude2',extensions='.json')
+            assert len(m2)==1
+            pd=layout.get(subject=sub,session=sess,
+                            type='phasediff',extensions='.json')
+            assert len(pd)==1
+            pd_metadata=layout.get_metadata(pd[0].filename)
+            if 'EchoTime' in pd_metadata:
+                pd_metadata['EchoTime1']=layout.get_metadata(m1[0].filename)['EchoTime']
+                pd_metadata['EchoTime2']=layout.get_metadata(m2[0].filename)['EchoTime']
+                del pd_metadata['EchoTime']
+                json.dump(pd_metadata,open(pd[0].filename,'w'),
+                                sort_keys=True,indent=4)
 
         boldruns=layout.get(subject=sub,session=sess,
                     type='bold',extensions='.json')
