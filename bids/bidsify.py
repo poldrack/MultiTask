0cab7748da42a704e190ac9230b6704a864cebfc
poldrack <poldrack@gmail.com>
2018-01-05 16:53:43 -0800

    working version

--- bids/bidsify.py
+++ bids/bidsify.py
@@ -2,9 +2,10 @@
 convert multitask data to BIDS
 """
 
-import os,sys
+import os,sys,shutil
 import glob
 import json
+import datetime
 
 if os.path.exists('bids_commands.sh'):
     os.remove('bids_commands.sh')
@@ -23,7 +24,7 @@ else:
     #basedir='/scratch/01329/poldrack/MultiTask'
     basedir='/Users/poldrack/data_unsynced/multitask'
 
-verbose=True
+verbose=False
 
 datainfo={}
 
@@ -42,6 +43,7 @@ def getFileInfo(niifile,bidsbase,setcode,
     fileinfo['sessnum']=int(os.path.basename(fdir).split('_')[1])
     fileinfo['seriesnum']=fname_s[-1]
     fileinfo['bidssub']='sub-%02d'%fileinfo['subnum']
+    fileinfo['bidssubdir']=os.path.join(bidsbase,fileinfo['bidssub'])
     fileinfo['bidssess']='ses-%d'%fileinfo['sessnum']
     fileinfo['bidssubdir']=os.path.join(bidsbase,fileinfo['bidssub'])
     fileinfo['bidssessdir']=os.path.join(fileinfo['bidssubdir'],fileinfo['bidssess'])
@@ -50,14 +52,39 @@ def getFileInfo(niifile,bidsbase,setcode,
             os.mkdir(fileinfo['bidssubdir'])
         if not os.path.exists(fileinfo['bidssessdir']):
             os.mkdir(fileinfo['bidssessdir'])
-    if fname_s[2]=='gre':
+    # skip equalized files
+    if fname.find('Eq_1')>-1:
+        print('skipping equalized file')
+        fileinfo['filetype']='unknown'
+        sd=None
+    elif fname_s[2]=='gre':
         fileinfo['filetype']='fmap'
         sd=os.path.join(fileinfo['bidssessdir'],'fmap')
+        dcmhdr=json.load(open(niifile.replace('nii.gz','json')))
+        fileinfo['echoTime']=dcmhdr['EchoTime']
+        if dcmhdr['ImageType'][2]=='P':
+            fileinfo['fmaptype']='phasediff'
+        else:
+            if dcmhdr['EchoTime']<0.005:
+                fileinfo['fmaptype']='magnitude1'
+            else:
+                fileinfo['fmaptype']='magnitude2'
+        fileinfo['bidsfilestem']=os.path.join(sd,
+                    '%s_%s_%s'%(fileinfo['bidssub'],
+                        fileinfo['bidssess'],
+                        fileinfo['fmaptype']))
+
     elif fname_s[2]=='mprage':
         fileinfo['filetype']='anat'
         sd=os.path.join(fileinfo['bidssessdir'],'anat')
         fileinfo['bidsfilestem']=os.path.join(sd,
                     '%s_%s_T1w'%(fileinfo['bidssub'],
+                        fileinfo['bidssess']))
+    elif fname_s[2]=='tse':
+        fileinfo['filetype']='anat'
+        sd=os.path.join(fileinfo['bidssessdir'],'anat')
+        fileinfo['bidsfilestem']=os.path.join(sd,
+                    '%s_%s_T2w'%(fileinfo['bidssub'],
                         fileinfo['bidssess']))
 
     elif fname_s[2]=='ep':
@@ -111,10 +138,11 @@ for setcode in ['SC1','SC2']:
             datainfo[niifile]=fileinfo
             if verbose:
                 print(fileinfo)
+            if not 'bidsfilestem' in fileinfo:
+                print('skipping bids commands')
+                continue
             niifilestem=niifile.replace('.nii.gz','')
-            if fileinfo['filetype']=='anat':
-                print('processing anat file')
-                for ft in ['.nii.gz','.json']:
-                    cmd='cp %s%s %s%s'%(niifilestem,ft,
+            for ft in ['.nii.gz','.json']:
+                cmd='cp %s%s %s%s'%(niifilestem,ft,
                                     fileinfo['bidsfilestem'],ft)
-                    saveCommand(cmd)
+                saveCommand(cmd)
